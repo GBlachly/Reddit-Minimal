4b76ce08ce2d937ec6abc2471be0d74bba979ed6
GBlachly <gblachly4@gmail.com>
2022-11-01 20:48:30 -0400

    font edits to App

--- src/App.js
+++ src/App.js
@@ -7,13 +7,23 @@ import { Subreddits } from './containers/Subreddits/Subreddits';
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
-        <h1>Welcome to my app</h1> 
+        <h1>Welcome to my app</h1>
+        <nav>
+          <ul>
+            <li>HOME</li>
+            <li>REDDIT</li>
+            <li>SUBREDDITS</li>
+          </ul>
+        </nav> 
       </header>
+      
       <main className='App-main-grid'>
         <Subreddits /> 
         {/* <Posts /> */}
       </main>
+
     </div>
   );
 }
