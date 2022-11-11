a7d08bcd3806a7dd431bc89ae61a35c5e4e465f1
GBlachly <gblachly4@gmail.com>
2022-11-11 15:14:49 -0500

    Up/down vote arrows change color and affect score

--- src/components/post/post.js
+++ src/components/post/post.js
@@ -1,4 +1,4 @@
-import React from 'react'; 
+import React, { useState, useEffect } from 'react'; 
 import './post.css';
 import { useSelector } from 'react-redux';
 
@@ -10,6 +10,37 @@ import {selectSearchTerm} from '../../containers/Posts/postsSlice';
 export const Post = (props) => {
     const { post, index, onToggleComments } = props;
     const searchTerm = useSelector(selectSearchTerm);
+    const [upVoted, setUpVoted] = useState(false);
+    const [downVoted, setDownVoted] = useState(false);
+    const [score, setScore] = useState(Number(post.score));
+
+    useEffect(()=>{
+      setUpVoted(false);
+      setDownVoted(false);
+      setScore(Number(post.score));
+    }, [post]);
+
+    const handleUpVote = () => {
+      setUpVoted(prev => !prev);
+      if (!upVoted) {
+        setScore(Number(post.score) + 1)
+      };
+      if (upVoted) {
+        setScore(Number(post.score))
+      };
+      setDownVoted(false);
+    };
+
+    const handleDownVote = () => {
+      setDownVoted(prev => !prev);
+      if (!downVoted) {
+        setScore(Number(post.score) - 1)
+      };
+      if (downVoted) {
+        setScore(Number(post.score))
+      };
+      setUpVoted(false);
+    };
 
     const renderComments = () => {
         if (post.errorComments) {
@@ -51,9 +82,9 @@ export const Post = (props) => {
         <div className='Post-individual'>
           <div className='post-no-comments' >
               <div className='post-karma'>
-                <TiArrowUpOutline className='vote-icon' />
-                <h1>{post.score}</h1>
-                <TiArrowDownOutline className='vote-icon' />
+                <TiArrowUpOutline className='vote-icon-up' style={upVoted && {color: 'green'} } onClick={handleUpVote} />
+                <h1>{score}</h1>
+                <TiArrowDownOutline className='vote-icon-down' style={downVoted && {color: 'red'} } onClick={handleDownVote}/>
               </div>
 
               <div className='post-title' >
