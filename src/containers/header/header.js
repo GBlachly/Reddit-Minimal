71889375cb9c45a4b99cf8098d1a4eca6a1d51fb
GBlachly <gblachly4@gmail.com>
2022-11-18 12:12:10 -0500

    Edited <title>/search buttons smallest screen CSS

--- src/containers/header/header.js
+++ src/containers/header/header.js
@@ -37,8 +37,8 @@ export const Header = () => {
         <section className='header-search-bar' >
             <form onSubmit={handleSubmit}>
                 <input id='search-bar-input' type='text' value={localSearchTerm} onChange={handleChange}/>
-                <input className='search-buttons' type='submit' value='Search Posts'/>
-                <button className='search-buttons' onClick={clearSearch} >Clear Search</button>
+                <input className='search-buttons' id='search-btn' type='submit' value='Search Posts'/>
+                <button className='search-buttons' id='clear-btn' onClick={clearSearch} >Clear Search</button>
             </form>
         </section>
     </header>
